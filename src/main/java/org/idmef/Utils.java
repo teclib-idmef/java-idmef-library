b1d27c23beeea746a5349c7ce4711ea719e29ccd
François Déchelle <fdechelle@teclib.com>
2021-10-19 14:27:23 +0200

    add Utils class

---
+++ src/main/java/org/idmef/Utils.java
@@ -0,0 +1,27 @@
+package org.idmef;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Utils {
+
+    private static final String BASE = '/' + Utils.class.getPackage().getName().replace(".", "/");
+
+    public static JsonNode loadResource(String path) throws IOException {
+        InputStream inputStream = Utils.class.getClassLoader().getResourceAsStream(BASE + '/' + path);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.readTree(inputStream);
+    }
+
+    public static JsonNode loadString(String content) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        return mapper.readTree(content);
+    }
+}
