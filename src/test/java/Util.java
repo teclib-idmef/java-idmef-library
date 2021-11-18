38dc89cfb8e93d4ef130f55177134191e9060f7e
François Déchelle <fdechelle@teclib.com>
2021-11-18 17:00:26 +0100

    Add second test JSON string

--- src/test/java/Util.java
+++ src/test/java/Util.java
@@ -1,4 +1,4 @@
-import org.idmef.Analyzer;
+import org.idmef.IDMEFObject;
 import org.idmef.Message;
 
 import java.time.ZoneOffset;
@@ -21,14 +21,13 @@ public class Util {
         msg.put("Version", "2.0.3");
         msg.put("ID", uuid());
         msg.put("CreateTime", now());
-        Analyzer analyzer = new Analyzer();
+        IDMEFObject analyzer = new IDMEFObject(msg, "Analyzer");
         analyzer.put("IP", "127.0.0.1");
         analyzer.put("Name", "foobar");
         analyzer.put("Model", "generic");
         analyzer.put("Category", new String[]{"LOG"});
         analyzer.put("Data", new String[]{"Log"});
         analyzer.put("Method", new String[]{"Monitor"});
-        msg.put("Analyzer", analyzer);
 
         return msg;
     }
@@ -47,5 +46,23 @@ public class Util {
                     "\"Name\":\"foobar\"\n" +
                     "}\n" +
                 "}\n";
+    }
+
+    static String string2() {
+        return "{\"Version\": \"2.0.3\",\n" +
+                " \"ID\": \"e2f2cef4-25fb-4a24-a47a-cf88aa2b8cd8\",\n" +
+                " \"CreateTime\": \"2021-11-18T16:43:48.606197\",\n" +
+                " \"Analyzer\": {\"IP\": \"127.0.0.1\",\n" +
+                "\t      \"Name\": \"foobar\",\n" +
+                "\t      \"Model\": \"generic\",\n" +
+                "\t      \"Category\": [\"LOG\"],\n" +
+                "\t      \"Data\": [\"Log\"],\n" +
+                "\t      \"Method\": [\"Monitor\"]},\n" +
+                " \"Sensor\": [{\"IP\": \"192.168.1.1\",\n" +
+                "\t     \"Name\": \"TheSensor\",\n" +
+                "\t     \"Model\": \"TheSensorModel\"},\n" +
+                "\t    {\"IP\": \"192.168.1.2\",\n" +
+                "\t     \"Name\": \"TheSensor2\",\n" +
+                "\t     \"Model\": \"TheSensor2Model\"}]}\n";
     }
 }
