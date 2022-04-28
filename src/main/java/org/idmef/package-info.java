8ca0bcf5faf694d8134b317f455a282131b54a76
François Déchelle <fdechelle@teclib.com>
2022-04-28 11:53:54 +0200

    Generate PDF javadoc

---
+++ src/main/java/org/idmef/package-info.java
@@ -0,0 +1,15 @@
+package org.idmef;
+
+/**
+ * Package org.idmef contains the implementation of the IDMEF V2 format specification.
+ *
+ * This package implements:
+ * <ul>
+ *     <li>message implementation in class IDMEFObject</li>
+ *     <li>message serialization and deserialization to/from JSON</li>
+ *     <li>message validation against current JSON schema of the IDMEF V2 specification</li>
+ * </ul>
+ *
+ * @see https://www.secef.net/faq-idmef/
+ *
+ */
